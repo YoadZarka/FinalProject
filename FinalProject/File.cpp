6e73bfcf82f310d7b7884e812980e9920ee27838
yoad <yoad@ubuntu>
2017-04-03 04:04:43 -0700

    initial checking

---
+++ FinalProject/File.cpp
@@ -0,0 +1,20 @@
+/*
+ * File.cpp
+ *
+ *  Created on: Apr 3, 2017
+ *      Author: yoad
+ */
+
+#include "File.h"
+
+File::File() {
+	// TODO Auto-generated constructor stub
+
+
+}
+
+File::~File() {
+	// TODO Auto-generated destructor stub
+
+}
+
---
+++ FinalProject/File.h
@@ -0,0 +1,17 @@
+/*
+ * File.h
+ *
+ *  Created on: Apr 3, 2017
+ *      Author: yoad
+ */
+
+#ifndef FILE_H_
+#define FILE_H_
+
+class File {
+public:
+	File();
+	virtual ~File();
+};
+
+#endif /* FILE_H_ */
